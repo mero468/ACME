494b3f19b30ad8bf660b7ce4794b585b203727b7
mero468 <ammarkaid321@gmail.com>
2022-02-26 02:43:01 +0300

    More backend and some updates on the look of the page

--- events/models.py
+++ events/models.py
@@ -4,8 +4,18 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=100)
+    login = models.CharField(max_length=1000)
     id = models.CharField(max_length=100).primary_key
     avatar = models.CharField(max_length=1000)
-    language = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     url = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000)
+
+class Candidates(models.Model):
+    name = models.CharField(max_length=100)
+    login = models.CharField(max_length=1000)
+    id = models.CharField(max_length=100).primary_key
+    avatar = models.CharField(max_length=1000)
+    location = models.CharField(max_length=100)
+    url = models.CharField(max_length=1000)
+    email = models.CharField(max_length=1000,null=True)
--- events/views.py
+++ events/views.py
@@ -3,7 +3,7 @@ import json
 import requests
 from django.shortcuts import render, redirect
 
-from events.models import User
+from events.models import Candidates
 
 
 # Create your views here.
@@ -17,15 +17,14 @@ def error(request):
 
 
 def candidates(request):
-    users = User.objects.all()
+    users = Candidates.objects.all()
     return render(request, 'candidates.html', {'Users_list': users})
 
 def delete_cand(request,user_id):
-    object = User.objects.get(pk=user_id)
+    object = Candidates.objects.get(pk=user_id)
     object.delete()
-    users = User.objects.all()
-    return render(request, 'candidates.html', {'Users_list': users})
-
+    users = Candidates.objects.all()
+    return redirect('home')
 def result(request):
     # We Get the form's value
     if request.method == 'GET':
@@ -45,6 +44,7 @@ def result(request):
 
         while (count < 9):
             count += 1
+            #search api
             url = 'https://api.github.com/search/users?q=' + username + '+location:' + location + '+language:' + language + '+repos:%3E' + repos + '&page=' + str(
                 count) + '&per_page=100'
             r = requests.get(url)
@@ -59,21 +59,33 @@ def result(request):
             if "total_count" in data2:
                 x = data2['total_count'] - (count * 100)
                 if x <= 0:
-                    flag = False
-                for item in data2['items']:
-                    name = item['login']
-                    id = item['id']
-                    avatar = item['avatar_url']
-                    url = item['html_url']
-                    user = User(name=name, id=id, avatar=avatar, url=url, language=language, location=location)
+                    # User github api
+                    url2 = 'https://api.github.com/users/' + username
+                    r = requests.get(url2)
+                    data = r.json()
+                    jsonString = json.dumps(data)
+                    with open('json_userdata.json', 'w') as outfile:
+                        outfile.write(jsonString)
+                    outfile.close()
+                    with open('json_userdata.json', 'r') as readfile:
+                        data3 = json.load(readfile)
+                    readfile.close()
+                    login = data3['login']
+                    id = data3['id']
+                    avatar = data3['avatar_url']
+                    url = data3['html_url']
+                    email = data3['email']
+                    name = data3['name']
+                    location = data3['location']
+                    user = Candidates(login=login ,name=name, email=email, id=id, avatar=avatar, url=url, location=location)
                     user.save()
                     list_user.append(user)
             else:
                 if "message" in data2:
                     return render(request, "error.html", {})
                 else:
-                    return render(request, "result.html", {'Users_list': list_user})
-        return render(request, "result.html", {'Users_list': list_user})
+                    return render(request, "result.html", {'Users_list': set(list_user)})
+        return render(request, "result.html", {'Users_list': set(list_user)})
 
 
     else:
